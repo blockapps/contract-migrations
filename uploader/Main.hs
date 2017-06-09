15011246380bee821ae442ececc592091dfba334
Martin Allen <martin.allen26@gmail.com>
2017-06-09 17:48:14 -0400

    refactor

--- uploader/Main.hs
+++ uploader/Main.hs
@@ -4,7 +4,7 @@ module Main where
 
 import           BlocMigrations
 import           BuildArtifacts
-import           Control.Error
+import qualified Control.Error as E
 import           Control.Lens ((^.))
 import           Control.Monad
 import           Data.Monoid
@@ -14,19 +14,21 @@ import           System.Environment (lookupEnv)
 
 main :: IO ()
 main = do
-  eRes <- runExceptT $ do
-    admin <- getAdminFromEnv
-    bloc <- makeBlocEnv
-    yml <- lookupEnv "CONTRACTS_YAML" !? EnvError "Couldn't find CONTRACTS_YAML"
-    cDir <- lookupEnv "CONTRACTS_DIR" !? EnvError "Couldn't find CONTRACTS_DIR"
-    buildRoot <- lookupEnv "BUILD_ROOT" !? EnvError "Couldn't find BUILD_ROOT"
+  eRes <- E.runExceptT $ do
+    cfg <- mkMigrationConfig
+    buildRoot <- lookupEnv "BUILD_ROOT" E.!? EnvError "Couldn't find BUILD_ROOT"
     let buildDir = buildRoot <> "/build"
-    results <- ExceptT $ runMigration bloc admin yml cDir
-    _ <- liftIO $ removePathForcibly buildDir
-    liftIO . print $ ("Writing Admin Details" :: String)
-    _ <- liftIO $ writeAdmin buildDir (results^.migrationAdminAddress)
-    artifacts <- forM (results^.migrationContractList) $ getContractAbis bloc (buildDir <> "/contracts")
-    liftIO $ forM_ (mconcat artifacts) writeArtifact
+    return (cfg, buildDir)
   case eRes of
     Left e -> putFailure e
-    Right () -> putSuccess ("Build Artifacts written!" :: String)
+    Right (cfg, buildDir) -> do
+      eMigrationRes <- runMigrator cfg $ do
+        results <- runMigration
+        _ <- liftIO $ removePathForcibly buildDir
+        liftIO . print $ ("Writing Admin Details" :: String)
+        _ <- liftIO $ writeAdmin buildDir (results^.migrationAdminAddress)
+        artifacts <- forM (results^.migrationContractList) $ getContractAbis (buildDir <> "/contracts")
+        liftIO $ forM_ (mconcat artifacts) writeArtifact
+      case eMigrationRes of
+        Left e -> putFailure e
+        Right () -> putSuccess ("Build Artifacts written!" :: String)
